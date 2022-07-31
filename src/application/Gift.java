0e66089947d9b2db8156d37ace97823bf4328cfb
DanaNewtonGunderson <dana.newtongunderson@ucalgary.ca>
2022-07-30 22:36:42 -0600

    Added minAge and maxAge as instance variables and created getter methods

--- src/application/Gift.java
+++ src/application/Gift.java
@@ -3,14 +3,16 @@ package application;
 import java.util.ArrayList;
 
 /**
- * The Gift class represents gift objects. Gifts can have a name, price, targeted gender, and interest categories
- * that the gift is associated with (e.g. sports, games, etc). Each gift can have 1 to 3 different interests
- * that it is categorized under. The rateGift method can be called to rate the quality of a gift for a specific 
+ * The Gift class represents gift objects. Gifts can have a name, price, targeted gender, minimum and maximum age,
+ * and interest categories the gift is associated with (e.g. sports, games, etc). Each gift can be categorized under
+ * 1 to 3 different interests. The rateGift method can be called to rate the quality of a gift for a specific 
  * person based on the person's rating of the gift's interest categories. 
  *
  */
 public class Gift {
 	private String name;
+	private int minAge;
+	private int maxAge;
 	private int price;
 	private char gender;
 	private double giftRating;
@@ -20,12 +22,16 @@ public class Gift {
 	 * Gift constructor for gifts that are classified under 1 category/ interest.
 	 * 
 	 * @param name the name/description of the gift
+	 * @param minAge the minimum age of a possible recipient of this gift
+	 * @param maxAge the maximum age of a possible recipient of this gift
 	 * @param price the price of the gift
 	 * @param gender 'F' if gift is female, 'M' if gift is male, 'O' if gift is other
 	 * @param giftCategory Interest that categorizes gift
 	 */
-	public Gift (String name, int price, char gender, Interest giftCategory) {
+	public Gift (String name, int minAge, int maxAge, int price, char gender, Interest giftCategory) {
 		this.name = name;
+		this.minAge = minAge;
+		this.maxAge = maxAge;
 		this.price = price;
 		this.gender = gender;
 		giftCharacteristics = new ArrayList<Interest>();
@@ -36,13 +42,18 @@ public class Gift {
 	 * Gift constructor for gifts that are classified under 2 categories/ interests.
 	 * 
 	 * @param name the name/description of the gift
+	 * @param minAge the minimum age of a possible recipient of this gift
+	 * @param maxAge the maximum age of a possible recipient of this gift
 	 * @param price the price of the gift
 	 * @param gender 'F' if gift is female, 'M' if gift is male, 'O' if gift is other
 	 * @param giftCategory1 first Interest that categorizes gift
 	 * @param giftCategory2 second Interest that categorizes gift
 	 */
-	public Gift (String name, int price, char gender, Interest giftCategory1, Interest giftCategory2) {
+	public Gift (String name, int minAge, int maxAge, int price, char gender, 
+			Interest giftCategory1, Interest giftCategory2) {
 		this.name = name;
+		this.minAge = minAge;
+		this.maxAge = maxAge;
 		this.price = price;
 		this.gender = gender;
 		giftCharacteristics = new ArrayList<Interest>();
@@ -54,14 +65,19 @@ public class Gift {
 	 * Gift constructor for gifts that are classified under 3 categories/ interests.
 	 * 
 	 * @param name the name/description of the gift
+	 * @param minAge the minimum age of a possible recipient of this gift
+	 * @param maxAge the maximum age of a possible recipient of this gift
 	 * @param price the price of the gift
 	 * @param gender 'F' if gift is female, 'M' if gift is male, 'O' if gift is other
 	 * @param giftCategory1 first Interest that categorizes gift
 	 * @param giftCategory2 second Interest that categorizes gift
 	 * @param giftCategory3 third Interest that categorizes gift
 	 */
-	public Gift (String name, int price, char gender, Interest giftCategory1, Interest giftCategory2, Interest giftCategory3) {
+	public Gift (String name, int minAge, int maxAge, int price, char gender, 
+			Interest giftCategory1, Interest giftCategory2, Interest giftCategory3) {
 		this.name = name;
+		this.minAge = minAge;
+		this.maxAge = maxAge;
 		this.price = price;
 		this.gender = gender;
 		giftCharacteristics = new ArrayList<Interest>();
@@ -116,5 +132,21 @@ public class Gift {
 	 */
 	public double getGiftRating() {
 		return giftRating;
+	}
+
+	/**
+	 * Returns the youngest age a person can be to be recommended this gift
+	 * @return the minAge
+	 */
+	public int getMinAge() {
+		return minAge;
+	}
+
+	/**
+	 * Returns the oldest age a person can be to be recommended this gift
+	 * @return the maxAge
+	 */
+	public int getMaxAge() {
+		return maxAge;
 	}
 }
