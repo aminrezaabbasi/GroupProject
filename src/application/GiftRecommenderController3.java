505fca68c956f234d43a35ab548ab3ae3e0379b2
DanaNewtonGunderson <dana.newtongunderson@ucalgary.ca>
2022-08-04 22:16:05 -0600

    Set gift label to gift and added button to get another gift suggestion

--- src/application/GiftRecommenderController3.java
+++ src/application/GiftRecommenderController3.java
@@ -1,15 +1,33 @@
 package application;
 
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class GiftRecommenderController3 {
-	private Person user;
+	private ArrayList<Gift> personalizedGiftList;
+	int index = 0;
 	
 	@FXML
 	private Label giftLabel;
 	
-	public void setUser(Person person) {
-    	user = person;
-    }
+
+	public void getGift(Person user) {
+		GiftList gifts = new GiftList();
+		personalizedGiftList = gifts.getSortedGiftList(user);
+		giftLabel.setText(personalizedGiftList.get(index).toString());
+		
+	}
+	
+	
+	@FXML
+	public void getNewGift(ActionEvent event) {
+		if (index < personalizedGiftList.size() - 1)
+			index++;
+		else
+			index = 0;
+		giftLabel.setText(personalizedGiftList.get(index).toString());
+	}
 }
